633d9f66ef4f16948981e6836d3c881b8bb95da0
ak-online <79262567597@yandex.com>
2024-08-28 15:10:39 +0300

    Add files via upload

--- module_4_1.py
+++ module_4_1.py
@@ -1,13 +1,13 @@
 import true_math as tm
 import fake_math as fm
-import random
+from random import randint
 
-n1 = random.randint(21,40)
-n2 = random.randint(1,10)
+n1 = randint(21,40)
+n2 = randint(1,10)
 print(tm.divide(n1, n2))
 print(tm.divide(n1, 0))
 
-n1 = random.randint(n1,100)
-n2 = random.randint(n2,30)
+n1 = randint(n1,100)
+n2 = randint(n2,30)
 print(fm.divide(n1, n2))
 print(fm.divide(n1, 0))
